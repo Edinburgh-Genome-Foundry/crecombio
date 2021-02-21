ee969bb3e73e177af970027e01805525e607b756
Peter Vegh <pvegh@posteo.uk>
2021-02-21 13:53:53 +0000

    Test count_number_of_sites()

--- test_crecombio.py
+++ test_crecombio.py
@@ -6,6 +6,20 @@ from Bio.Seq import Seq
 import crecombio
 
 
+def test_count_number_of_sites():
+    seq_with_three_sites = SeqRecord(
+        Seq(
+            "GACTGATGTGACGTGTGACAGCTGACGAAGTTCCTATTCTCTAGAAAGTATAGGAACTTCAAAAAAAAAAAAA"
+            "AAAAAAAAAGAAGTTCCTATTCTCTAGAAAGTATAGGAACTTCCCCCCCCCCCCCCCCCCCCCCCCCCCC"
+            "GGGAAGTTCCTATACTTTCTAGAGAATAGGAACTTCC"
+        )
+    )
+    site = crecombio.SITES["Flp"]["seq"]
+    matches, rc_matches = crecombio.count_number_of_sites(seq_with_three_sites, site)
+    assert matches == [26, 82]
+    assert rc_matches == [145]
+
+
 def test_recombine_one_sequence():
     excision_seq = SeqRecord(
         Seq(
