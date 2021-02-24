9e791d2d5a62dc129cb4069218dabe86b017a31b
Peter Vegh <pvegh@posteo.uk>
2021-02-24 22:06:58 +0000

    Set up Travis CI

--- crecombio/__init__.py
+++ crecombio/__init__.py
@@ -1,3 +1,9 @@
-from .crecombio import SITES, recombine, recombine_one_sequence, recombine_two_sequences
+from .crecombio import (
+    SITES,
+    recombine,
+    recombine_one_sequence,
+    recombine_two_sequences,
+    count_number_of_sites,
+)
 
 from .version import __version__
